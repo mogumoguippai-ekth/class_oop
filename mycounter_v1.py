a2c95648dd7da1e519730b3bcb6c8d5c002af879
mogumoguippai-ekth <mogumogu.ippai@gmail.com>
2025-09-26 13:18:51 +0900

    課題D-4作成

---
+++ mycounter_v1.py
@@ -0,0 +1,33 @@
+class MyCounterV1:
+
+    # カウンタ初期値を初期化する
+    def __init__(self, value):
+        self.value = value
+
+    # カウンタ初期入力値を返す
+    def value(self):
+        return self.value
+    
+    # カウンタを1つ増やす
+    def count_up(self):
+        self.value += 1
+        return
+
+
+counter1 = MyCounterV1(value=0)
+print(counter1.value)  # 0
+
+counter1.count_up()
+print(counter1.value)  # 1
+
+counter1.count_up()
+print(counter1.value)  # 2
+
+counter2 = MyCounterV1(value=7)
+print(counter2.value)  # 7
+
+counter2.count_up()
+print(counter2.value)  # 8
+
+counter2.count_up()
+print(counter2.value)  # 9
